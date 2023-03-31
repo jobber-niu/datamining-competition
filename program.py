ae750146f33c5d7c2546688edd38b1a99077c771
jober-niu <nyjoxfine@163.com>
2023-03-31 11:05:48 +0800

    ing...

--- program.py
+++ program.py
@@ -2,5 +2,16 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt0 = pd.read_csv('order_train0.csv')
-dt0.head(5)
+d = pd.read_csv('./示例数据/order_train0.csv')
+d.head(5)
+# d.iloc[:,0:1]
+d.query("sales_region_code == 101")
+d.info()
+c = d['item_price']
+max(c)
+print(d.sales_region_code.unique())
+print(d['item_code'].unique().size)
+
+s = sum(d['item_price'])
+print(s)
+print(d['item_price']/s)
