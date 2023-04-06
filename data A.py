7ea2b258fed5fac535d22f690f4670e9b0075b28
jober-niu <nyjoxfine@163.com>
2023-04-06 21:47:27 +0800

    23.4.6 测试

--- data A.py
+++ data A.py
@@ -1,17 +1,24 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn
+# 基本库
 
-d = pd.read_csv('./示例数据/order_train0.csv')
-d.head(5)
-# d.iloc[:,0:1]
-d.query("sales_region_code == 101")
-d.info()
-c = d['item_price']
-max(c)
-print(d.sales_region_code.unique())
-print(d['item_code'].unique().size)
+df = pd.read_csv('./示例数据/order_train0.csv')
+df.head(5)
+df.info()
+df['order_date'] = pd.to_datetime(df['order_date'])
 
-s = sum(d['item_price'])
-print(s)
-print(d['item_price']/s)
+plt.figure(figsize=(10,5))
+print(df.item_price.describe())
+plt.subplot(1,2,1)
+plt.plot(df['order_date'],df['item_price'])
+print(df.ord_qty.describe())
+plt.subplot(1,2,2)
+plt.plot(df['order_date'],df['ord_qty'])
+plt.show()
+
+dirc = {'offline':0,'online':1}
+df['sales_chan_name'].replace(dirc,inplace = True)
+
+# 更新
